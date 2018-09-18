8ed038cdc19cc90e781a0b8008616a9d04083e6f
Allie Webb <alliewebb@Allies-MacBook-Pro.local>
2018-09-18 16:27:25 +1200

    fixed bug in checkForWin function

--- minesweeper.js
+++ minesweeper.js
@@ -5,7 +5,7 @@ var board = {
   cells : [
     {row: 0, 
       col:0, 
-      isMine:false, 
+      isMine:true, 
       hidden:true
     }, 
     {row:1, 
@@ -52,7 +52,7 @@ var board = {
 }
 
 function startGame () {
-  for (i = 0; i < board.cells.length; i++) {
+  for (var i = 0; i < board.cells.length; i++) {
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
   }
   document.addEventListener("click", checkForWin);
@@ -66,15 +66,15 @@ function startGame () {
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin () {
-  for (i =0; i <board.cells.length; i++) {
+  for (var i = 0; i < board.cells.length; i++) {
     if (board.cells[i].isMine && !board.cells[i].isMarked) {
       return
     }
     if (!board.cells[i].isMine && board.cells[i].hidden) {
       return
     }
-  return lib.displayMessage('You win!');
     }
+    return lib.displayMessage('You win!')
   }
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
@@ -91,7 +91,7 @@ function checkForWin () {
 function countSurroundingMines (cell) {
   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
   var count = 0
-  for (i = 0; i < surrounding.length; i++) {
+  for (var i = 0; i < surrounding.length; i++) {
     if (surrounding[i].isMine === true) {
       count++
     }
